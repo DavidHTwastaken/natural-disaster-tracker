b38ae3f2a155773356f621302a7c46d7594f8ef4
DavidHTwastaken <david.houletymeczko@ontariotechu.net>
2023-07-31 19:58:45 -0400

    fixed header. gonna change menu next

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -5,8 +5,8 @@ import spinner from "/public/loading.gif";
 const Loader = () => {
   return (
     <div className={styles.loader}>
-      <Image src={spinner} alt="Loading" width={250} height={250} />
       <h1>Fetching data...</h1>
+      <Image src={spinner} alt="Loading" width={250} height={250} />
     </div>
   );
 };
