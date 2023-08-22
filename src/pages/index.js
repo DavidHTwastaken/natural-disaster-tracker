55a2a327f8407ea63d651cb3919c2ebc3f6aad1a
DavidHTwastaken <david.houletymeczko@ontariotechu.net>
2023-08-22 17:16:04 -0400

    Changed favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,7 +59,7 @@ export default function Home() {
           content="An interactive map showing natural events around the world."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
       <Header menu={[menuOpen, setMenuOpen]} />
       {loading ? (
